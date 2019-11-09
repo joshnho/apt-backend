64ff9cd60ef98f3fbf87a4ef545fcf27a34e47d9
Josh Nho <joshik@Joshs-MBP.lan1>
2019-11-09 10:25:50 -0500

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :listings, through: :appointments
+
+    # validates :username, presence: true
+    # validates :username, uniqueness: true
 end
